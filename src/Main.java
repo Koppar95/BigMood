3bd1955917d305f02d5251c177372efb10fce1c1
Teo <becerra.teo@gmail.com>
2020-02-04 22:35:15 +0100

    added emojis and effects

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.Glow;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 public class Main extends Application {
 //TESTING
@@ -17,17 +25,67 @@ public class Main extends Application {
         window = primaryStage;
         window.setTitle("Big Mood");
 
+        //Effect for smiley hoover
+        DropShadow shadow = new DropShadow();
+        Glow glow = new Glow();
+
         VBox leftMenu = new VBox(15);
         Button moodButton = new Button("mood");
         Button userButton = new Button("users");
         leftMenu.getChildren().addAll(moodButton,userButton);
 
+
+        Image happy = new Image("/happy.png");
+        Image sad = new Image("sad.png");
+        ImageView happyView = new ImageView(happy);
+        ImageView sadView = new ImageView(sad);
+
+        happyView.setOnMouseEntered(e -> {
+            happyView.setEffect(shadow);
+        });
+
+        happyView.setOnMouseExited(e -> {
+            happyView.setEffect(null);
+        });
+
+        happyView.setOnMouseClicked(e -> {
+            happyView.setEffect(glow);
+        });
+
+        sadView.setOnMouseEntered(e -> {
+            sadView.setEffect(shadow);
+        });
+
+        sadView.setOnMouseExited(e -> {
+            sadView.setEffect(null);
+        });
+
+        sadView.setOnMouseClicked(e -> {
+            sadView.setEffect(glow);
+        });
+
+
+        TextField userComment = new TextField();
+        Label userLabel = new Label("Comment:");
+
         VBox centerLayout = new VBox();
 
+        HBox comment = new HBox();
+        comment.getChildren().addAll(userLabel, userComment);
+        comment.setAlignment(Pos.CENTER);
+
+        HBox emojis = new HBox();
+        emojis.getChildren().addAll(happyView,sadView);
+        emojis.setAlignment(Pos.CENTER);
+
+
+        centerLayout.getChildren().addAll(emojis,comment);
+        centerLayout.setAlignment(Pos.CENTER);
 
         BorderPane mainLayout = new BorderPane();
         mainLayout.setLeft(leftMenu);
         mainLayout.setCenter(centerLayout);
+        mainLayout.setStyle("-fx-background-color: linear-gradient(#E4EAA2, #9CD672);");
 
         primaryStage.setScene(new Scene(mainLayout, 600, 550));
         primaryStage.show();
