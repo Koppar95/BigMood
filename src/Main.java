78b1f4c04550796089c40622bf6600c54d258a9f
Teo <becerra.teo@gmail.com>
2020-02-04 15:46:17 +0100

    added left menu

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,39 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Stage window;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        window = primaryStage;
+        window.setTitle("Big Mood");
 
-        LoginBox.display("Login");
+        VBox leftMenu = new VBox();
+        Button moodButton = new Button("mood");
+        Button userButton = new Button("users");
+        leftMenu.getChildren().addAll(moodButton,userButton);
+
+        VBox centerLayout = new VBox();
+
+
+        BorderPane mainLayout = new BorderPane();
+        mainLayout.setLeft(leftMenu);
+        mainLayout.setCenter(centerLayout);
+
+        primaryStage.setScene(new Scene(mainLayout, 600, 550));
+        primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
-
-
 }
