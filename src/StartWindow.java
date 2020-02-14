d1e17da5bc9f953f0df360cb39fe6357b4dfde9d
Teo <becerra.teo@gmail.com>
2020-02-14 13:37:37 +0100

    "Polished startwindow"

--- src/StartWindow.java
+++ src/StartWindow.java
@@ -16,23 +16,19 @@ public class StartWindow extends VBox {
         StartWindow start= new StartWindow();
         String family = "Helvetica";
         double size = 40;
-        //
-        Button userCheck = new Button("Which user is active????");
-        userCheck.setOnAction(e->System.out.println(LoginBox.currentUser.get("Email")));
-        //
+
         TextFlow textFlow = new TextFlow();
         textFlow.setLayoutX(40);
         textFlow.setLayoutY(40);
-        Text text1 = new Text("Welcome ");
+        Text text1 = new Text("Welcome " + LoginBox.currentUser.get("Name") + "!");
         text1.setFont(Font.font(family, size));
         text1.setFill(Color.DARKGREEN);
         text1.setStroke(Color.rgb(0,0,0,0.2));
         textFlow.getChildren().addAll(text1);
         textFlow.setTextAlignment(TextAlignment.CENTER);
 
-        textFlow.setTextAlignment(TextAlignment.CENTER);
-        start.getChildren().addAll(textFlow, userCheck);
-        start.setAlignment(Pos.CENTER);
+        start.getChildren().addAll(textFlow);
+        start.setAlignment(Pos.TOP_CENTER);
 
         return start;
     }
