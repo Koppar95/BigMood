d24dabeb868fb35fe2500ed8b5c4fe8755d29756
karlsvenssonn <B27ojo95>
2020-02-18 21:11:31 +0100

    Added methods to create label and textfields. With this I could remove code duplicates. Also added a Cancel button if user wants to go back to login screen.

--- src/RegisterBox.java
+++ src/RegisterBox.java
@@ -21,14 +21,48 @@ import java.time.LocalDate;
  * @since 2020-02-18
  */
 public class RegisterBox {
-
-    public interface Delegate {
-        public void registerUser();
-        public void cancel();
+    /**
+     * Creates a new TextField for user input
+     * @param promptText promptText to be displayed in field.
+     * @param column Which column in the GridPane.
+     * @param row Which row in the GridPane
+     * @return The new TextField
+     */
+    private static TextField addTextField(String promptText, int column, int row){
+        TextField textField = new TextField();
+        textField.setPromptText(promptText);
+        textField.setMaxSize(200, 5);
+        GridPane.setConstraints(textField,column,row);
+        return textField;
     }
 
-    public Delegate delegate;
+    /**
+     * Creates a Label
+     * @param title The title of the Label to be displayed.
+     * @param column Which column in the GridPane.
+     * @param row which row in the GridPane.
+     * @return The new label.
+     */
+    private static Label addLabel(String title, int column, int row){
+        Label label = new Label(title);
+        GridPane.setConstraints(label,column, row);
+        return label;
+    }
 
+    /**
+     * Creates a new PasswordField for user input of password.
+     * @param promptText promptText to be displayed in field.
+     * @param column Which column in the GridPane.
+     * @param row Which row in the GridPane.
+     * @return The new PasswordField.
+     */
+    private static PasswordField addPassField(String promptText, int column, int row){
+        PasswordField passwordField = new PasswordField();
+        passwordField.setPromptText(promptText);
+        passwordField.setMaxSize(200,5);
+        GridPane.setConstraints(passwordField, column, row);
+        return passwordField;
+    }
     /**
      * Creates the RegisterBox screen with all TextFields and Labels.
      * Checks valid inputs.
@@ -53,72 +87,26 @@ public class RegisterBox {
         grid.setVgap(8);
         grid.setHgap(10);
 
-        //Instructions
-        Label instructionsLabel = new Label();
-        instructionsLabel.setText("");
-        GridPane.setConstraints(instructionsLabel,1,2);
-
-        //Username Label
-        Label userLabel = new Label("Username:");
-        GridPane.setConstraints(userLabel, 0,3);
-
-        //Username Input
-        TextField userInput = new TextField();
-        userInput.setPromptText("Enter Username");
-        userInput.setMaxSize(200,5);
-        GridPane.setConstraints(userInput,1,3);
-
-        //Password Label
-        Label passwordLabel = new Label("Password:");
-        GridPane.setConstraints(passwordLabel, 0,4);
-
-        //Password Input
-        PasswordField passwordInput = new PasswordField();
-        passwordInput.setPromptText("Enter Password");
-        GridPane.setConstraints(passwordInput, 1,4);
-        passwordInput.setMaxSize(200,5);
-
-        //Password Label 2
-        Label passwordLabel2= new Label("Re-Enter Password:");
-        GridPane.setConstraints(passwordLabel2, 0,5);
-
-        //Password Input 2
-        PasswordField passwordInput2 = new PasswordField();
-        passwordInput2.setPromptText("Re-Enter Password");
-        GridPane.setConstraints(passwordInput2, 1,5);
-        passwordInput.setMaxSize(200,5);
-
-        //Name Label
-        Label nameLabel = new Label("Name:");
-        GridPane.setConstraints(nameLabel, 0,6);
-
-        //Name Input
-        TextField nameInput = new TextField();
-        nameInput.setPromptText("Enter your Name");
-        nameInput.setMaxSize(200,5);
-        GridPane.setConstraints(nameInput, 1,6);
-
-        //Age Label
-        Label ageLabel = new Label("Date of Birth:");
-        GridPane.setConstraints(ageLabel, 0,7);
-
+        //Labels for input fields
+        Label usernameLabel = addLabel("Username:", 0,3);
+        Label passwordLabel = addLabel("Password:", 0,4);
+        Label passwordLabel2 = addLabel("Re-Enter Password:", 0,5);
+        Label nameLabel = addLabel("Name:", 0,6);
+        Label dobLabel = addLabel("Date of Birth:", 0,7);
+        Label heightLabel = addLabel("Height:", 0,8);
+        Label wrongHeight = addLabel("", 2,8);
+        Label warningLabel = addLabel("", 2,5);
+        //Input fields
+        TextField userInput = addTextField("Enter Username", 1,3);
+        TextField nameInput = addTextField("Enter your name",1,6);
+        TextField heightInput = addTextField("Enter your height",1,8);
+        PasswordField passwordInput = addPassField("Enter Password", 1,4);
+        PasswordField passwordInput2 = addPassField("Re-Enter Password", 1,5);
         //Date of birth choiceBox
         DatePicker datePicker = new DatePicker();
         GridPane.setConstraints(datePicker, 1,7);
 
-        //Height Label
-        Label heightLabel = new Label("Height:");
-        GridPane.setConstraints(heightLabel, 0, 8);
-
-        //Height Input
-        TextField heightInput = new TextField();
-        heightInput.setPromptText("Enter your height");
-        heightInput.setMaxSize(200,5);
-        GridPane.setConstraints(heightInput,1,8);
-
         /* Check correct input of height */
-        Label wrongHeight = new Label();
-        GridPane.setConstraints(wrongHeight, 2,8);
         heightInput.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
@@ -131,13 +119,12 @@ public class RegisterBox {
                 }
             }
         });
-
+        Button cancelButton = new Button("Cancel");
+        GridPane.setConstraints(cancelButton, 2,10);
         Button registerButton = new Button("Register");
         GridPane.setConstraints(registerButton, 1,10);
 
         /* Check that passwords matches */
-        Label warningLabel = new Label();
-        GridPane.setConstraints(warningLabel, 2, 5);
         passwordInput2.focusedProperty().addListener(new ChangeListener<Boolean>() {
             @Override
             public void changed(ObservableValue<? extends Boolean> observableValue, Boolean aBoolean, Boolean t1) {
@@ -169,12 +156,6 @@ public class RegisterBox {
 
         /* Register the new user when pressing button */
         registerButton.setOnAction(e-> {
-            /*
-            if (delegate != null) {
-                delegate.registerUser();
-            }
-             */
-
             if(registerUser(userInput, passwordInput, nameInput, datePicker.getValue(), heightInput)) {
                 window.close();
             }
@@ -183,11 +164,13 @@ public class RegisterBox {
             }
         });
 
+        cancelButton.setOnAction(e-> window.close());
+
         grid.getChildren().addAll(
-                userLabel,userInput,passwordLabel,
+                usernameLabel,userInput,passwordLabel,
                 passwordInput, passwordLabel2,passwordInput2,nameLabel, nameInput,
-                ageLabel, datePicker, heightLabel,
-                heightInput,registerButton, instructionsLabel, warningLabel, wrongHeight
+                dobLabel, datePicker, heightLabel,
+                heightInput,registerButton, warningLabel, wrongHeight, cancelButton
         );
 
         Undecorator undecorator = new Undecorator(window,grid);
