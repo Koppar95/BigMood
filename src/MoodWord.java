4c36ab24523d3290f7d539473683e3ed106c7f3c
Teo <becerra.teo@gmail.com>
2020-03-01 21:06:38 +0100

    "Polished startwindow"

---
+++ src/MoodWord.java
@@ -0,0 +1,27 @@
+public class MoodWord {
+    private String word;
+    private Integer count;
+
+    public MoodWord(String word, Integer count){
+        this.word = word;
+        this.count = count;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+
+}
