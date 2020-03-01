2d9abbbf0906b8c819308754569475cff860a581
Teo <becerra.teo@gmail.com>
2020-03-01 21:08:16 +0100

    "Polished startwindow"

--- src/MoodHashMap.java
+++ src/MoodHashMap.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 
+//We'll structure everything regarding the hashmap here instead of the datawindow
 public class MoodHashMap {
     private HashMap<String, Integer> moodHashMap;
 
